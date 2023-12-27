d08a5dd255bffae22a00e56ab9c4820f9b0ae213
dagemov <sebcoss1234@gmail.com>
2023-12-27 09:19:37 -0500

    Date Class Finichs(console)

---
+++ ConceptsPOO/ConceptsPOO/Date.cs
@@ -0,0 +1,58 @@
+﻿namespace ConceptsPOO
+{
+    public class Date
+    {
+        private int _day { get; set; }
+        private int _month { get; set; }
+        private int _year { get; set; }
+
+        public Date(int year, int month, int day)
+        {
+            _day = CheckDay(year, month, day);
+            _month = CheckMonth(month);
+            _year = year;
+        }
+
+        private int CheckDay(int year, int month, int day)
+        {
+            if (month == 2 && day == 29 && IsLeapYear(year))
+            {
+                return day;
+            }
+            int[] daysPerMomth = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+            if (day >= 1 && day <= daysPerMomth[month])
+            {
+                return day;
+            }
+            throw new DayException("Invalid Day");
+        }
+
+        private bool IsLeapYear(int year)
+        {
+            return year%400 ==0  || year%4==0 && year%100!=0;
+            //if (year % 4 == 0 && year % 100 ! = 0 && year % 400==0)
+            //{
+            //    return true;
+            //}
+            //else
+            //{
+            //    return false;
+            //}
+        }
+
+        private int CheckMonth(int month)
+        {
+            if (month >= 1 && month <= 12)
+            {
+                return month;
+            }
+            throw new MonthException("Invalid Month");
+        }
+
+        public override string ToString()
+        {
+            return $"{_year}/{_month:00}/{_day:00}";
+        }
+
+    }
+}
---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+using ConceptsPOO;
+
+Console.WriteLine("**** { POO CONCEPTS } ****");
+
+//Date date = new Date(2022,2,11);
+
+try
+{
+	Console.WriteLine(new Date(2022, 2, 11));
+
+	Console.WriteLine(new Date(2022, 12, 29));
+
+	Console.WriteLine(new Date(2021, 8, 18));
+
+	Console.WriteLine(new Date(2012, 1, 14));
+
+	Console.WriteLine(new Date(2001, 9, 11));
+
+	Console.WriteLine(new Date(2003, 2,28));
+}
+catch (Exception error)
+{
+
+	Console.WriteLine(error.Message);
+}
+
