cf6121f489cccfc45024aa571f086890d0adb972
dagemov <sebcoss1234@gmail.com>
2024-01-09 18:21:34 -0500

    create Database and stringConnection

---
+++ Twins/TwinsShared/Entities/Country.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Twins.Shared.Entities
+{
+    public class Country
+    {
+        public int Id { get; set; }
+        [MaxLength(100, ErrorMessage = "The field {0} don't can have more {1} caracters")]
+        [Required(ErrorMessage = "The filed {0} is required")]
+        [Display(Name = "Country Name")]
+        public string Name { get; set; } = null!;
+    }
+}
